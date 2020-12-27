20a206bc7f75b3f51489821b39ca042a1d745e10
Jordan Marta <jordan.smta@gmail.com>
2020-12-27 13:53:03 -0300

    'AdicionandoAngular'

--- ProAgil.WebAPI/Startup.cs
+++ ProAgil.WebAPI/Startup.cs
@@ -30,6 +30,7 @@ namespace ProAgil.WebAPI
             services.AddDbContext<DataContext>(x => x
                 .UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +42,7 @@ namespace ProAgil.WebAPI
             }
 
             // app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseRouting();
 
@@ -50,6 +52,7 @@ namespace ProAgil.WebAPI
             {
                 endpoints.MapControllers();
             });
+
         }
     }
 }
